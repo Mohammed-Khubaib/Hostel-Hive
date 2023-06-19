3e9ca777432666e7aba50b79fb545e04cbd04e38
Khubaib <mohmmedkhubaib2001@gmail.com>
2023-06-19 21:45:23 +0530

    v-1 Stable Version of HotelHive

--- HostelBookingWebsite/HostelHive/views.py
+++ HostelBookingWebsite/HostelHive/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render ,HttpResponse
-
+from HostelHive.models import Booknow
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -10,10 +11,28 @@ def home(request):
     return HttpResponse("This is the home page")
 def about(request):
     return render(request,'about.html')
-    # return HttpResponse("About page")
 def pricing(request):
     return render(request,'pricing.html')
 def booknow(request):
-    # return HttpResponse("booknow page")
+    if request.method == "POST" :
+        name = request.POST.get('name')
+        country_zip = request.POST.get('country_zip')
+        checkin =  request.POST.get('checkin')
+        checkout = request.POST.get('checkout')
+        rooms = request.POST.get('rooms')
+        adults = request.POST.get('adults')
+        children = request.POST.get('children')
+        email = request.POST.get('email')
+        phonenumber = request.POST.get('phonenumber')
+        bknow = Booknow(name = name , country_zip = country_zip, checkin = checkin , checkout = checkout, rooms = rooms  ,adults=adults, children = children , email=email , phonenumber = phonenumber)
+        bknow.save()
+        # i want this messages to print if and only if the data is saved without anyissues
+        try:
+            bknow.save()
+            messages.warning(request, "Your Reservation is Successfully Registered")
+            messages.success(request, "We will shortly contact you through your email or phone number")
+        except Exception as e:
+            messages.error(request, f"An error occurred while saving the reservation: {str(e)}")
     return render(request,'booknow.html')
     
+
